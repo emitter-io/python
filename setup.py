f19dda499b5355edbf2c124d9fbdfbe0e01da50d
Florian <flokX@users.noreply.github.com>
2018-11-09 17:33:35 +0100

    Increase development status

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://emitter.io",
     packages=["emitter"],
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Topic :: Communications",
         "Topic :: Internet :: WWW/HTTP",
