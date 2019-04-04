331487214cc278b4e2185c1a43339840699e9920
Florimond Husquinet <florimond@husquinet.net>
2019-04-04 19:39:21 +0200

    sample share

--- emitter/sample-python3.py
+++ emitter/sample-python3.py
@@ -4,9 +4,19 @@ import json
 
 emitter = Client()
 
+root = tkinter.Tk()
+
+channel_key = tkinter.StringVar(root, value="8jMLP9F859oDyqmJ3aV4aqnmFZpxApvb")
+#channel_key = tkinter.StringVar(root, value="EckDAy4LHt_T0eTPSBK_0dmOAGhakMgI")#local
+channel = tkinter.StringVar(root, value="test/")
+shortcut = tkinter.StringVar(root, value="a0")
+text_message = tkinter.StringVar(root, value="Hello World")
+share_group = tkinter.StringVar(root, value="sg")
+share_group_key = tkinter.StringVar(root, value="qQrtann17qNi3CTwW7N8F4OR9uAuQBHw")
+
 def connect():
-	options = {"host": "192.168.0.10", "secure": False}
-	#options = {"secure": False}
+	#options = {"host": "127.0.0.1", "secure": False}
+	options = {"secure": False}
 	emitter.connect(options)
 
 	emitter.on_connect = lambda: result_text.insert("0.0", "Connected\n\n")
@@ -22,7 +32,7 @@ def disconnect():
 	emitter.disconnect()
 
 def subscribe(share_group=None):
-	str_key = emitter_key.get()
+	str_key = channel_key.get()
 	str_channel = channel.get()
 	emitter.subscribe(str_key,
 	 str_channel,
@@ -30,57 +40,59 @@ def subscribe(share_group=None):
 	 share_group=share_group)
 	result_text.insert("0.0", "Subscribtion to '" + str_channel + "' requested.\n\n")
 
+def subscribe_share():
+	str_key = share_group_key.get()
+	str_channel = channel.get()
+	str_share = share_group.get()
+	emitter.subscribe(str_key,
+	 str_channel,
+	 optional_handler=lambda m: result_text.insert("0.0", "Message received on handler for " + str_channel + ": " + m.as_string() + "\n\n"),
+	 share_group=str_share)
+	result_text.insert("0.0", "Subscribtion to '" + str_channel + "' requested.\n\n")
+
 def unsubscribe():
-	str_key = emitter_key.get()
+	str_key = channel_key.get()
 	str_channel = channel.get()
 	emitter.unsubscribe(str_key, str_channel)
 	result_text.insert("0.0", "Unsubscribtion to '" + str_channel + "' requested.\n\n")
 
 def presence():
-	str_key = emitter_key.get()
+	str_key = channel_key.get()
 	str_channel = channel.get()
 	emitter.presence(str_key, str_channel)
 	result_text.insert("0.0", "Presence on '" + str_channel + "' requested.\n\n")
 
 def message(retain=False):
-	emitter.publish(emitter_key.get(), channel.get(), text_message.get(), retain=retain)
+	emitter.publish(channel_key.get(), channel.get(), text_message.get(), retain=retain)
 	result_text.insert("0.0", "Test message send through '" + channel.get() + "'.\n\n")
 
 def link():
-	str_key = emitter_key.get()
+	str_key = channel_key.get()
 	str_channel = channel.get()
 	str_link = shortcut.get()
 	emitter.link(str_key, str_channel, str_link, False, True)
 
 def link_private():
-	str_key = emitter_key.get()
+	str_key = channel_key.get()
 	str_channel = channel.get()
 	str_link = shortcut.get()
 	emitter.link(str_key, str_channel, str_link, True, True)
 
 def pub_to_link():
 	str_link = shortcut.get()
-	emitter.publish_with_link(str_link, json.dumps({"key1": "value1", "key2": 2}))
+	emitter.publish_with_link(str_link, text_message.get())
 
 def me():
 	emitter.me()
 
-root = tkinter.Tk()
-#emitter_key = tkinter.StringVar(root, value="8jMLP9F859oDyqmJ3aV4aqnmFZpxApvb")
-emitter_key = tkinter.StringVar(root, value="EckDAy4LHt_T0eTPSBK_0dmOAGhakMgI")#local
-channel = tkinter.StringVar(root, value="test/")
-shortcut = tkinter.StringVar(root, value="a0")
-text_message = tkinter.StringVar(root, value="Hello World")
-share_group = tkinter.StringVar(root, value="sg")
-
 # Col 1
-tkinter.Label(root, text="Emitter key : ").grid(column=1, row=1)
-emitter_key_entry = tkinter.Entry(root, width=40, textvariable=emitter_key)
-emitter_key_entry.grid(column=1, row=2)
-
-tkinter.Label(root, text="Channel : ").grid(column=1, row=3)
+tkinter.Label(root, text="Channel : ").grid(column=1, row=1)
 channel_entry = tkinter.Entry(root, width=40, textvariable=channel)
-channel_entry.grid(column=1, row=4)
+channel_entry.grid(column=1, row=2)
+
+tkinter.Label(root, text="Channel key : ").grid(column=1, row=3)
+channel_key_entry = tkinter.Entry(root, width=40, textvariable=channel_key)
+channel_key_entry.grid(column=1, row=4)
 
 tkinter.Label(root, text="Shortcut : ").grid(column=1, row=5)
 shortcut_entry = tkinter.Entry(root, width=40, textvariable=shortcut)
@@ -93,6 +105,10 @@ message_entry.grid(column=1, row=8)
 tkinter.Label(root, text="Share group : ").grid(column=1, row=9)
 share_entry = tkinter.Entry(root, width=40, textvariable=share_group)
 share_entry.grid(column=1, row=10)
+
+tkinter.Label(root, text="Share group key : ").grid(column=1, row=11)
+share_key_entry = tkinter.Entry(root, width=40, textvariable=share_group_key)
+share_key_entry.grid(column=1, row=12)
 
 
 # Col 2
@@ -108,8 +124,8 @@ subscribe_button.grid(column=2, row=4)
 unsubscribe_button = tkinter.Button(root, text="Unsubscribe", width=30, command=unsubscribe)
 unsubscribe_button.grid(column=2, row=5)
 
-subscribe_button = tkinter.Button(root, text="Subscribe to share", width=30, command=lambda: subscribe(share_group.get()))
-subscribe_button.grid(column=2, row=6)
+subscribe_share_button = tkinter.Button(root, text="Subscribe to share", width=30, command=subscribe_share)
+subscribe_share_button.grid(column=2, row=6)
 
 # Col 3
 link_button = tkinter.Button(root, text="Link to shortcut", width=30, command=link)
@@ -137,6 +153,6 @@ me_button.grid(column=4, row=2)
 
 # Text area
 result_text = tkinter.Text(root, height=30, width=120)
-result_text.grid(column=1, row=12, columnspan=4)
+result_text.grid(column=1, row=14, columnspan=4)
 
 root.mainloop()
