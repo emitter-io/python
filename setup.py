11e59e41a0039763ca4c540801bde11adfac9b9b
Florimond Husquinet <florimond@husquinet.net>
2020-05-24 15:03:25 +0200

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="emitter-io",
-    version="2.0.3",
+    version="2.1.0",
     author="Florimond Husquinet",
     author_email="florimond@emitter.io",
     description="A Python library to interact with the Emitter API.",
